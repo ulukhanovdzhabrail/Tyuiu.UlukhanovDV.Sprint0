8f61944302fc4f73f175fb326c9cd7288cfea059
ulukhanovdzhabrail <djabrail192007@mail.ru>
2025-09-10 02:20:32 +0500

    Task1

---
+++ Tyuiu.UlukhanovDV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Джабраил";
+        int age = 17;
+        Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
