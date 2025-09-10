f3e0c74f3a10f16cf2199684e30974d23a8b9ca2
ulukhanovdzhabrail <djabrail192007@mail.ru>
2025-09-10 23:56:22 +0500

    Task4

---
+++ Tyuiu.UlukhanovDV.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.UlukhanovDV.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.UlukhanovDV.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            Console.WriteLine(DataService.Division(5, 5));
+            Console.ReadKey();
+        }
+    }
+}
