491e0184fe5fd9b20ebe24e3871508f0af640b23
ulukhanovdzhabrail <djabrail192007@mail.ru>
2025-09-11 03:17:03 +0500

    Task7

---
+++ Tyuiu.UlukhanovDV.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Tyuiu.UlukhanovDV.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = numOne[i] + numTwo[i];
+            }
+            return resultArray;
+        }
+    }
+}
