ccb75426e5068552ded3a23f67688a10fdd663df
ulukhanovdzhabrail <djabrail192007@mail.ru>
2025-09-10 21:38:49 +0500

    Task3

---
+++ Tyuiu.UlukhanovDV.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Tyuiu.UlukhanovDV.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.UlukhanovDV.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
